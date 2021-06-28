2d82f6bc5f651639ad4b7ef055f47fac08799cce
Kyle-Atienza <atienzakylefrancis@gmail.com>
2021-06-28 22:15:49 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+console.log("connected")
+
+var offer_name = document.querySelector("#offers-name>h1")
+var offers = document.querySelector("#offers-images").children
+
+for (let i = 0; i < offers.length; i++) {
+    offers[i].addEventListener("mouseover", function(){
+        //console.log(isHover)
+        //console.log(offers[i])
+        var offer_item = offers[i].dataset.name
+        switch (offer_item){
+            case 'pizza':
+                offer_name.innerHTML = "Pizza"
+                break;
+            case 'burger':
+                offer_name.innerHTML = "Burger"
+                break;
+            case 'pasta':
+                offer_name.innerHTML = "Pasta"
+                break;
+            case 'quencher':
+                offer_name.innerHTML = "Quencher"
+                break;
+            case 'shawarma':
+                offer_name.innerHTML = "Shawarma"
+                break;
+        }
+    })
+    
+}
+
+
